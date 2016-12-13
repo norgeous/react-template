4c9bc16e8c80552d56863f51c3df8ebc5a311d0e
Daniel Harte <daniel@harte.me>
2016-12-13 14:16:33 +0000

    `npm start` now spawns chrome on windows

--- devserver.js
+++ devserver.js
@@ -1,15 +1,28 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var exec = require('child_process').exec
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
+	publicPath: config.output.publicPath,
+	hot: true,
+	historyApiFallback: true
 }).listen(3000, '0.0.0.0', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
 
-  console.log('Listening at http://0.0.0.0:3000/');
+	if (err) {
+		return console.log(err);
+	}
+
+	console.log('Listening at http://0.0.0.0:3000/');
+
+	exec('start chrome http://localhost:3000' , function(err) {
+
+		if(err) {
+			console.log(err)
+		} else {
+			console.log("chrome opened")
+		}
+
+	})
+
 });
